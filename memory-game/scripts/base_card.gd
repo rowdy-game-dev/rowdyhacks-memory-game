2f0772794b582e4e205a17cecab03659e5ea8258
rowdy-game-dev <kristo.cisneros-valdez@my.utsa.edu>
2024-10-26 17:32:41 -0500

    change scenes

---
+++ memory-game/scripts/base_card.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var sprite: Node = $"sprite"
+@onready var collision_shape = $"CollisionShape2D"
+@export var size_scale: float = 4
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	sprite.scale = Vector2(size_scale, size_scale)
+	collision_shape.scale = Vector2(size_scale, size_scale)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
