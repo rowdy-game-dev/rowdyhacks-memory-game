4f827ee6a7dc0e1aaa8b774525c27ef14a4b036a
rowdy-game-dev <kristo.cisneros-valdez@my.utsa.edu>
2024-10-26 21:14:22 -0500

    refactored card scenes

--- memory-game/scripts/base_card.gd
+++ memory-game/scripts/base_card.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
 @onready var sprite: Node = $"sprite"
-@onready var area_2d: Area2D = $Area2D
+@onready var area_2d: Area2D = $"Area2D"
 @export var size_scale: float = 1
-@onready var timer: Timer = $Timer
+@onready var timer: Timer = $"Timer"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	scale = Vector2(size_scale, size_scale)
+	area_2d.input_event.connect(_on_area_2d_input_event)
+	timer.timeout.connect(_on_timer_timeout)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
